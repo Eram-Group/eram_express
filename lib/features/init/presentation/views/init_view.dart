ba54c080ca7125f11c00c4f3ca647fbb15def33b
Youssef al-Tai <youssefgalalaltai@gmail.com>
2024-10-12 21:23:56 +0300

    feat: build splash screen

--- lib/features/init/presentation/views/init_view.dart
+++ lib/features/init/presentation/views/init_view.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../../../app/di.dart';
 import 'init_view_model.dart';
@@ -6,18 +9,90 @@ import 'init_view_model.dart';
 class InitView extends StatelessWidget {
   static const String route = '/';
 
-  final viewModel =
-      InitViewModel(authenticationRepository: authenticationRepository);
+  final viewModel = InitViewModel(
+    authenticationRepository: authenticationRepository,
+    configurationsRepository: configurationsRepository,
+  );
+
   InitView({super.key}) {
     viewModel.init();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/splash.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 0,
+            right: 0,
+            child: SvgPicture.asset('assets/logo.svg'),
+          ),
+          const Positioned(
+            top: 280,
+            left: 0,
+            right: 0,
+            child: Spinner(),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class Spinner extends StatefulWidget {
+  const Spinner({
+    super.key,
+  });
+
+  @override
+  State<Spinner> createState() => _SpinnerState();
+}
+
+class _SpinnerState extends State<Spinner> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(
+            _animationController.value * 2 * pi,
+          )..translate(
+              0.0,
+              sin(_animationController.value * 2 * pi) * 6,
+            ),
+          child: child,
+        );
+      },
+      child: SvgPicture.asset('assets/brand-pin.svg'),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
--- lib/features/init/presentation/views/init_view_model.dart
+++ lib/features/init/presentation/views/init_view_model.dart
@@ -3,17 +3,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../app/di.dart';
 import '../../../authentication/domain/repositories/authentication_repository.dart';
 import '../../../authentication/presentation/views/screens/login/login_view.dart';
+import '../../../common/domain/repositories/configurations_repository.dart';
 import '../../../home/presentation/views/home_view.dart';
 
 class InitViewModel extends Cubit<bool> {
   final AuthenticationRepository _authenticationRepository;
+  final ConfigurationsRepository _configurationsRepository;
+
   InitViewModel({
     required AuthenticationRepository authenticationRepository,
+    required ConfigurationsRepository configurationsRepository,
   })  : _authenticationRepository = authenticationRepository,
+        _configurationsRepository = configurationsRepository,
         super(false);
 
   Future<void> init() async {
-    final isAuthenticated = await _authenticationRepository.isAuthenticated;
+    final results = await Future.wait([
+      _configurationsRepository.countries,
+      _authenticationRepository.isAuthenticated,
+    ]);
+
+    final isAuthenticated = results[1] as bool;
+
     if (isAuthenticated) {
       emit(true);
       mainNavigationService.clearStackAndNavigateTo(HomeView.route);
